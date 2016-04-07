6f45c61dc8d7c96676cbd7d6506d6beb80c14d66
Matheus Arleson Sales Xavier <matheusarleson@gmail.com>
2016-04-06 22:27:21 -0300

    improving bucket sort

--- src/main/java/br/com/xavier/algorithms/sort/impl/BucketSort.java
+++ src/main/java/br/com/xavier/algorithms/sort/impl/BucketSort.java
@@ -1,52 +1,39 @@
 package br.com.xavier.algorithms.sort.impl;
 
+import java.util.LinkedHashMap;
+
 import br.com.xavier.algorithms.sort.abstractions.AbstractSorter;
 
 public class BucketSort extends AbstractSorter {
 	
-	private int maxValue;
-	private int[] bucket;
+	private LinkedHashMap<Integer, Integer> map;
 	
 	public BucketSort() { }
 
 	@Override
 	public void doInitialization() {
-		// Determine minimum and maximum values
-        int minValue = arrayCopy[0];
-        this.maxValue = arrayCopy[0];
-        for (int i = 1; i < arraySize; i++) {
-            if (arrayCopy[i] < minValue) {
-            	minValue = arrayCopy[i];
-            } else if (arrayCopy[i] > this.maxValue) {
-            	this.maxValue = arrayCopy[i];
-            }
-        }
-
-        // Initialise bucket
-        this.bucket = new int[maxValue + 1];
+		map = new LinkedHashMap<>();
 	}
 
 	@Override
 	public int[] doSort(int[] array) {
-		//we are working with primitives, default is already zero
-		// init with zero count
-//		for (int i = 0; i < this.bucket.length; i++) {
-//			this.bucket[i] = 0;
-//		}
-
 		//trasverse the array
 		//each value is a bucket index 
 		//so, for each increment by one when a value is found
 		for (int i = 0; i < array.length; i++) {
-			this.bucket[array[i]]++;
+			int number = array[i];
+			if(map.containsKey(number)){
+				map.put(number, map.get(number) + 1);
+			} else {
+				map.put(number, 1);
+			}
 		}
-
+		
 		int pos = 0;
 		// for each bucket
 		// get the count and add the number times the count
-		//
-		for (int i = 0; i < this.bucket.length; i++) {
-			for (int j = 0; j < this.bucket[i]; j++) {
+		for (Integer number : map.keySet()) {
+			for (int i = 0; i < map.get(number); i++) {
 				array[pos++] = i;
 			}
 		}
